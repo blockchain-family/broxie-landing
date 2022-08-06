e4b27a148dafc900988c4072b2ded8000292e65d
Maksym Kudria <pretoriannbox@gmail.com>
2022-08-06 18:21:53 +0200

    navbar improved responsivness

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -5,14 +5,14 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='fixed z-10 top-4 lg:top-14 left-0 right-0 max-w-screen-xl mx-auto px-3 lg:px-8'>
+    <div className='fixed z-10 top-4 sm:top-8 lg:top-14 left-0 right-0 max-w-screen-xl mx-auto px-3 sm:px-5 lg:px-8'>
       <div className='flex justify-between items-center'>
         <div className='flex gap-5'>
-          <BroxieLogo className='w-24 lg:w-48' />
+          <BroxieLogo className='min-w-[5rem] sm:min-w-[7rem] lg:min-w-[12rem]' />
           <Broxie className='hidden lg:block' />
         </div>
 
-        <div className='flex items-center gap-2 sm:gap-4 lg:gap-8'>
+        <div className='flex items-center gap-4 sm:gap-8'>
           <NavLink to='/' className='no-underline'>
             Home
           </NavLink>
