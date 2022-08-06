629fd33d6a0dab5d1b4d2bb61e741266a9695804
Maksym Kudria <pretoriannbox@gmail.com>
2022-08-06 17:16:12 +0200

    scroll to top on router page change

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,13 @@ import Navbar from './layout/navbar';
 import Footer from './layout/footer';
 import LandingPage from 'modules/landing';
 import GalleryPage from 'modules/gallery';
+import ScrollToTop from './layout/scroll-to-top';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <Navbar />
 
       <Routes>
