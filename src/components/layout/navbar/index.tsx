2c1a8f1f6c4aba412817d5ebe07a9f1553803e2e
Maksym Kudria <pretoriannbox@gmail.com>
2022-08-11 17:16:01 +0200

    updated tailwind color variables to support opacity + navbar background

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,12 +1,12 @@
 import Button from 'components/core/button/button';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Broxie } from 'assets/images/broxie.svg';
 import { ReactComponent as BroxieLogo } from 'assets/images/broxie-logo.svg';
-import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='fixed z-10 top-4 sm:top-8 lg:top-14 left-0 right-0 max-w-screen-xl mx-auto px-3 sm:px-5 lg:px-8'>
-      <div className='flex justify-between items-center'>
+    <div className='fixed z-10 top-0 left-0 right-0 max-w-screen-xl mx-auto px-2 pt-4'>
+      <div className='flex justify-between items-center bg-background/25 rounded-2xl p-2 sm:p-3 lg:px-6'>
         <div className='flex space-x-5'>
           <BroxieLogo className='w-20 sm:w-28 lg:w-48 h-auto' />
           <Broxie className='hidden lg:block' />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      primary: 'var(--color-primary)',
-      primaryBg: 'var(--color-primaryBg)',
-      secondaryBg: 'var(--color-secondaryBg)',
-      hoverBg: 'var(--color-hoverBg)',
-      hover: 'var(--color-hover)',
-      link: 'var(--color-link)',
-      black: 'var(--color-black)',
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      primaryBg: 'rgb(var(--color-primaryBg) / <alpha-value>)',
+      secondaryBg: 'rgb(var(--color-secondaryBg) / <alpha-value>)',
+      hoverBg: 'rgb(var(--color-hoverBg) / <alpha-value>)',
+      hover: 'rgb(var(--color-hover) / <alpha-value>)',
+      link: 'rgb(var(--color-link) / <alpha-value>)',
+      black: 'rgb(var(--color-black) / <alpha-value>)',
+      background: 'rgb(var(--color-background) / <alpha-value>)',
       transparent: 'transparent',
     },
     extend: {
